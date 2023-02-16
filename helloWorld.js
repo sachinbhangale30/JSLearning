768b6022f6296490acbb2ec89641e5f39e370f43
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-16 16:29:57 +0530

    created hello world
    created hello world

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,17 @@
-console.log("Mohit Sharma");
+console.log("Sachin Bhangale");
+console.log("29");
+console.log("Pune");
+
+var fullName="Sachin Bhangale"
+console.log(fullName);
+
+var city;
+console.log(city);
+city = "Pune";
+console.log(city);
+city = "Surat"
+console.log(city);
+
+
+var age="28";
+console.log(age);
