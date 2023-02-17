6aa077b5f0c1cb09835bd3ec4f34d4cb224d7b88
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-17 19:30:47 +0530

    Created 02_typeOf
    Created 02_typeOf

---
+++ 02_typeOf.js
@@ -0,0 +1,37 @@
+console.log("**********Step 1**************");
+console.log("----Before Intialization Data Type----");
+var bankName;
+console.log("Bank Name:",bankName);
+console.log("Data Type:",typeof bankName);
+console.log("----After Intialization Data Type----");
+bankName = "SBI Bank";
+console.log("Bank Name:",bankName);
+console.log("Data Type:",typeof bankName)
+
+console.log("**********Step 2**************");
+console.log("----Declared & Initialized On The Same Line----");
+var marks = 90;
+var typeofmarks=typeof marks;
+console.log("Marks:",marks);
+console.log("Data Type:",typeof marks);
+
+console.log("**********Step 3**************");
+console.log("----Before Intialization Data Type----");
+var isworking;
+console.log("IsWorking:",isworking);
+console.log("Data Type:",typeof isworking);
+console.log("----After Intialization Data Type----");
+isworking = true;
+console.log("IsWorking:",isworking);
+console.log("Data Type:",typeof isworking);
+
+console.log("**********Step 4**************");
+console.log("----Declared & Initialized On The Same Line----");
+var totalCount = "Hundred and Seven";
+console.log("Total Count:",totalCount);
+console.log("Data Type:",typeof totalCount);
+
+console.log("-----Updated Variable With New Value-----");
+var totalCount = 107;
+console.log("Total Count:",totalCount);
+console.log("Data Type:",typeof totalCount);
