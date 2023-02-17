6b2b91d3b22e4dd495c15b401b458017b306ff15
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-17 11:36:12 +0530

    Created 02_dataTypes
    Created 02_dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("typeof variable age:", age);
+console.log("typeof variable weight:", weight);
+console.log("typeof age is:",isMarried);
+console.log("typeof weight is:",isLazy);
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", typeof graduation)
+var city=null;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", typeof graduation)
