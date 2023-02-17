c2867d10a6cedda6841573def827ea430370768e
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-17 19:30:20 +0530

    Created 02_swapVariables
    Created 02_swapVariables

---
+++ 02_swapVariables.js
@@ -0,0 +1,36 @@
+console.log("**********Step 1**************");
+console.log("-----Swap Two Variables-----");
+console.log("-----Before Swap-----");
+var sweety = "sweety";
+var cutie = "cutie";
+console.log("Sweety:",sweety);
+console.log("Cutie:",cutie);
+console.log("-----After Swap-----");
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Sweety:",sweety);
+console.log("Cutie:",cutie);
+
+console.log("**********Step 2**************");
+console.log("-----Swap Three Variables-----");
+console.log("-----Before Swap-----");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Num1:",num1);
+console.log("Num2:",num2);
+console.log("Num3:",num3);
+
+console.log("-----After Swap-----");
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("Num1:",num1);
+console.log("Num2:",num2);
+console.log("Num3:",num3);
+
+
+
+
