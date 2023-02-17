a18b8bd2577970084bd33a6853912be94024f1fa
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-17 19:29:34 +0530

    Created 01_variableBasicsAssign
    Created 01_variableBasicsAssign

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,50 @@
+console.log("--------------String Data Type--------------");
+var myName = "Sachin Bhangale";
+var graduation = "MCA";
+var address = "Mumbai";
+
+var typeofmyName = typeof myName;
+
+console.log("My Name:",myName);
+console.log("Data Type:",typeofmyName);
+console.log("Graduation:",graduation);
+console.log("Data Type:",typeof graduation);
+console.log("Address:",address);
+console.log("Data Type:",typeof address);
+console.log("--------------Number Data Type--------------");
+var age = 28;
+var weight = 72;
+var height = 5.8;
+
+
+console.log("Age:",age,);
+console.log("Data Type:",typeof age);
+console.log("Weight:",weight);
+console.log("Data Type:",typeof weight);
+console.log("Height",height);
+console.log("Data Type:",typeof height);
+console.log("--------------Boolean Data Type--------------");
+var isTalented = true;
+var isLazy = false;
+var isMarried = false;
+console.log("Is Talented:",isTalented);
+console.log("Data Type:", typeof isTalented);
+console.log("Is Lazy:",isLazy);
+console.log("Data Type:", typeof isLazy);
+console.log("Is Married",isMarried);
+console.log("Data Type:", typeof isMarried);
+
+
+console.log("----Undefined Variables and Data Types----",);
+var fullName;
+console.log("Full Name:",fullName);
+console.log("Data Type:",typeof fullName);
+
+console.log("-----Null Variables and Data Types-----",);
+var city = null;
+console.log("City:",city);
+console.log("City:",typeof city);
+
+
+
+
