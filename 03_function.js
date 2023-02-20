ac47fa65eea549437b70abf6dfcb94db23fefa3d
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-20 09:39:26 +0530

    Created 03_function
    Created 03_function

---
+++ 03_function.js
@@ -0,0 +1,52 @@
+// Function with no arguments and no return value
+function showFUllName() {
+  console.log('My full name is:Sachin Ashok Bhangale')
+}
+showFUllName()
+
+// Function with arguments and no return value
+function showAge(age) {
+  console.log('My Age is:', age)
+}
+showAge(32)
+
+// Function with no arguments and return value
+
+function fullName() {
+  var name = 'Sachin Bhangale'
+  return name
+}
+var fName = fullName()
+console.log(fName)
+
+// Function with arguments and return value
+
+function sumOfNumbers(num1, num2, num3) {
+  var sum = num1 + num2 + num3
+  return sum
+}
+var sumResult = sumOfNumbers(10, 45, 79)
+console.log(sumResult)
+
+var num1 = 100
+var num2 = 200
+
+var str1 = 'Sweety' // Cutey
+var str2 = 'Cutey' // Sweety
+
+var name1 = 'Akshay'
+var name2 = 'Sachin'
+
+function swapVariables(value1, value2) {
+  console.log('Before swap:,value1,value2')
+  var temp = value1
+  value1 = value2
+  value2 = temp
+  console.log('After Swap: ', value1, value2)
+  return 'Swapping variables successfully completed'
+}
+var swapResult = swapVariables(num1, num2) // Function call or Function invoke
+console.log(swapResult)
+
+var swapResult2 = swapVariables(str1, str2)
+console.log(swapResult2)
