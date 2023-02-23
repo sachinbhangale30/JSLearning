dca7b388f02a8afafbe45a8b508a246f46b4b8e2
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-23 06:52:00 +0530

    Updated 03_function
    Updated 03_function

--- 03_function.js
+++ 03_function.js
@@ -4,49 +4,49 @@ function showFUllName() {
 }
 showFUllName()
 
-// // Function with arguments and no return value
-// function showAge(age) {
-//   console.log('My Age is:', age)
-// }
-// showAge(32)
+// Function with arguments and no return value
+function showAge(age) {
+  console.log('My Age is:', age)
+}
+showAge(32)
 
-// // Function with no arguments and return value
+// Function with no arguments and return value
 
-// function fullName() {
-//   var name = 'Sachin Bhangale'
-//   return name
-// }
-// var fName = fullName()
-// console.log(fName)
+function fullName() {
+  var name = 'Sachin Bhangale'
+  return name
+}
+var fName = fullName()
+console.log(fName)
 
-// // Function with arguments and return value
+// Function with arguments and return value
 
-// function sumOfNumbers(num1, num2, num3) {
-//   var sum = num1 + num2 + num3
-//   return sum
-// }
-// var sumResult = sumOfNumbers(10, 45, 79)
-// console.log(sumResult)
+function sumOfNumbers(num1, num2, num3) {
+  var sum = num1 + num2 + num3
+  return sum
+}
+var sumResult = sumOfNumbers(10, 45, 79)
+console.log(sumResult)
 
-// var num1 = 100
-// var num2 = 200
+var num1 = 100
+var num2 = 200
 
-// var str1 = 'Sweety' // Cutey
-// var str2 = 'Cutey' // Sweety
+var str1 = 'Sweety' // Cutey
+var str2 = 'Cutey' // Sweety
 
-// var name1 = 'Akshay'
-// var name2 = 'Sachin'
+var name1 = 'Akshay'
+var name2 = 'Sachin'
 
-// function swapVariables(value1, value2) {
-//   console.log('Before swap:,value1,value2')
-//   var temp = value1
-//   value1 = value2
-//   value2 = temp
-//   console.log('After Swap: ', value1, value2)
-//   return 'Swapping variables successfully completed'
-// }
-// var swapResult = swapVariables(num1, num2) // Function call or Function invoke
-// console.log(swapResult)
+function swapVariables(value1, value2) {
+  console.log('Before swap:,value1,value2')
+  var temp = value1
+  value1 = value2
+  value2 = temp
+  console.log('After Swap: ', value1, value2)
+  return 'Swapping variables successfully completed'
+}
+var swapResult = swapVariables(num1, num2) // Function call or Function invoke
+console.log(swapResult)
 
-// var swapResult2 = swapVariables(str1, str2)
-// console.log(swapResult2)
+var swapResult2 = swapVariables(str1, str2)
+console.log(swapResult2)
