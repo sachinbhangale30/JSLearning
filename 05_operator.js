7e92f342a1f18c0a5ba78c97676f70636baf9299
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-23 08:45:19 +0530

    Learnt Comparison Operator
    Learnt Comparison Operator

--- 05_operator.js
+++ 05_operator.js
@@ -38,6 +38,27 @@ console.log(10 > 20); // > Greater than
 var marks = 35;
 console.log(marks >= 35); // >= Greater than equal
 
+var marks = 70;
+var result = marks >=60 ? "Allow him for Interview" :"Dont Allow";
+console.log(result);
+
+var age = 20;
+var res = age>= 21 ? "true" : "false";
+console.log(res);
+
+console.log("Even or Odd");
+var myNumber = 7;
+// Even -Divide by 2 and reminder is 0
+var result =  myNumber%2==0 ? "Even" : "Odd";
+console.log(result);
+
+function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : " No Try next time" ;
+    return result;
+ }
+  var result = maleMarriageEligibility("Male", 25, "Billgates");
+  console.log(result);
+
 
 
 
