dae3d45c9c6da199b7b3b4ed25d04013e8910679
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-23 06:52:28 +0530

    Created 04_stringTemplate
    Created 04_stringTemplate

---
+++ 04_stringTemplate.js
@@ -0,0 +1,8 @@
+console.log(`You are "champ" man`);
+var fisrtName = "Sachin";
+var lastName = "Bhangale";
+var city = "Mumbai";
+
+console.log("First Name: ",fisrtName,"Last Name: ",lastName,"City: ",city);
+
+console.log(`First Name ${fisrtName} Last Name ${lastName} City ${city}`);
