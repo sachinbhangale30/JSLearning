8f9afbdadeb7b2f545d6ec119ec16dd309b4f23e
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-23 06:50:30 +0530

    Created 04_stringAssign01
    Created 04_stringAssign01

---
+++ 04_stringAssign01.js
@@ -0,0 +1,12 @@
+function stringBasic(){
+    console.log("My dream company:Apple")
+}
+stringBasic();
+var hobby1 = "Reading";
+var hobby2 = "Coding"
+var hobby3 = "Exploring New Things"
+console.log("My hobbies are ", hobby1 +",", hobby2+",", hobby3+".");
+var hobby1CharCount = hobby1.length;
+hobby2CharCount = hobby1.length;
+hobby3CharCount = hobby1. length;
+console.log("Total number of characters are", hobby1CharCount + hobby2CharCount + hobby3CharCount);
