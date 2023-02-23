4be93cd609684a923aea7d857ab9fa2da448c617
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-02-23 06:52:52 +0530

    created 05_operator
    created 05_operator

---
+++ 05_operator.js
@@ -0,0 +1,47 @@
+console.log(`====Arithmetic Operator====`);
+var num1 = 10;
+var num2 = 2;
+var result = num1 + num2;
+console.log(`Addition: ${result}`);
+
+var subResult = num1 - num2;
+console.log(`Subtraction: ${subResult}`);
+
+var multiResult = num1 * num2;
+console.log(`Multiplication: ${multiResult}`);
+
+var expoResult = num1 ** num2;
+console.log(`Exponential: ${expoResult}`);
+
+var divResult = num1/num2;
+console.log(`Division: ${divResult}`);
+
+var modResult = num1 % num2;
+console.log(`Modulus: ${modResult}`);
+
+var num = 10;
+num+=20; //  num = num + 20
+console.log(`Compound addition += ${num}`);
+
+console.log(`Comparison Operators`);
+var num3 = 10;
+console.log(num3==10);
+var num4 = "10";
+var num5 = 10; 
+console.log(num4==num5); // In backgroung process it 1st convert (implicit conversion) data type frm string to number & then compare it
+console.log(num4===num5);  // It won't go for implicit conversion
+
+console.log(num3 != num4);  //   ! Not
+
+console.log(10 > 20); // > Greater than
+ 
+var marks = 35;
+console.log(marks >= 35); // >= Greater than equal
+
+
+
+
+
+
+
+
