ab51963e3498a32b3792a06cdd2c2778c5656916
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-01 10:37:24 +0530

    Created 06_for_Assignment01
    Created 06_for_Assignment01

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,34 @@
+console.log(`1.Wap to print numbers from 5 to 15 by incrementing 1`);
+for (let index = 5; index <= 15;index = index+1) {
+    console.log(index);   
+}
+
+console.log(`2.Wap to print numbers from 50 to 40 by decrementing 1`);
+for (let index = 50; index >= 40; index=index-1) {
+    console.log(index);   
+}
+
+console.log(`3.Wap to find first 15 ODD numbers`);
+for (let index = 1; index <= 30; index=index+2) {
+    console.log(index); 
+}
+
+console.log(`4.Wap to find first 10 EVEN numbers`);
+for (let index = 0; index <=20 ; index = index+2) {
+console.log(index);
+}
+
+console.log(`5.Wap to print table of 5 `);
+for (let index = 5; index <= 50 ; index = index+5) {
+        console.log(index);
+}
+
+console.log(`6.Wap to print table of 10`);
+for (let index = 10; index <= 100 ; index = index+10) {
+    console.log(index); 
+}
+
+console.log(`7.Wap to print table of 10 in Reverse Order`);
+for (let index = 100; index >= 10 ; index = index-10) {
+    console.log(index);
+}
