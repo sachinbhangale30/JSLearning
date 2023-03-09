a154e4e1fda73b408be04c51eb844c7b0cb843a9
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-10 01:15:21 +0530

    Updated 09_object
    Updated 09_object

--- 09_object.js
+++ 09_object.js
@@ -1,14 +1,47 @@
+// Object Literals
 let student = {
     firstName: "Sachin",
     lastName: "Bhangale",
     isWorking: false,
     age: 32,
     collegeName: "ABC",
-    id: 123456
+    id: 123456,
+    address: {
+        street: "Wakad",
+        city: "Pune",
+        pin: 431204,
+    },
+    school: "Podar School",
+    friends: ["Bill","Steve","Elon"],
+    show: function() {
+        console.log("I am show () function");
+    },
+    addressDetails: function(){ 
+        return `Address is: Street ${this.address.street}, City ${this.address.city}, PIN ${this.address.pin}` 
+    }
 };
-console.log(student);
+
+student.marks = {
+    math: 80,
+    physics: 60,
+    drawing: 70
+}
+
+let resultAddress = student.addressDetails();
+console.log(resultAddress);
+
+student.show();
+
+
+console.log(student.friends[student.friends.length-1]);
+
+console.log(student.marks); 
 
 console.log(typeof student);
+
+console.log(typeof student.id);
+
+console.log(typeof student.address);
 
 // . Dot Notation
 console.log(student.firstName);
@@ -16,19 +49,31 @@ console.log(student.firstName);
 // [] Notation
 console.log(student["lastName"]);
 
+// Update Property
+student.collegeName = "COEP PUNE";
+
 console.log(student.age);
 
 // Add new property city = "Mumbai"
 student.city = "Mumbai";
-console.log(student);
-
+student.country = "India";
+console.log(student); 
 student.country = "India"
+// console.table(student);
+
+// Delete the property
+delete student.isWorking
 console.table(student);
 
-// Empty object
-let teachet ={};
+// // Empty object
+let teacher ={};
 
 teacher.firstName = "Mohit"
+console.table(teacher);
 
 
 
+console.log(student.address.city);
+
+student.address.pin = 431205;
+console.table(student);
