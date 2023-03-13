e28a138bbf76663496caf7b3c51a9d42c10a4f8e
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-13 11:46:57 +0530

    Updated 10_class
    Updated 10_class

--- 10_class.js
+++ 10_class.js
@@ -6,10 +6,25 @@ class Bank {
         this.ifsc = ifsc;
         this.interestRate = interestRate;
     }
-
+    showDetails(){
+        console.log(`Bank Details: ${this.bankName} ${this.location} ${this.account} ${this.ifsc} ${this.interestRate} `);
+    }
 }
 const sbiBank = new Bank("SBI Bank", "Wakad", "62514897564", "SBIN0004598",10.75 );
-console.log(sbiBank);
+sbiBank.showDetails();
 
 const axisBank = new Bank("Axis Bank", "Bajirao Road, Pune", "4597863258", "AXIS4567893", 10.50);
-console.log(axisBank);
+axisBank.showDetails();
+
+class Person {
+    constructor(fullName, city){
+        this.fullName = fullName;
+        this.city = city;
+    }
+}
+const personDhoni = new Person("MS Dhoni", "Ranchi");
+const personShubham = new Person("Shubham Gill", "Mumbai");
+
+console.log(personDhoni instanceof Person);
+// console.log(personShubham instanceof Bank); // false
+// console.log(sbiBank instanceof Bank);
