4713fb6b393e3c74a2ad832f3200ae6a14a3f004
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-15 19:10:21 +0530

    Created 08_array_adv
    Created 08_array_adv

---
+++ 08_array_adv.js
@@ -0,0 +1,25 @@
+const arrayOfNames = ["Anil",  "Sunil", "Sita", "Bill", "Elon"];
+const result = arrayOfNames.join(": ");
+console.log(result);
+console.log(typeof result);
+
+console.log(arrayOfNames.length);
+console.log("Resize an array");
+
+arrayOfNames.length = 3;
+console.log(arrayOfNames.length);
+console.log(arrayOfNames);
+
+arrayOfNames.length = 7;
+console.table(arrayOfNames);
+
+
+const array = [3, 4, 5, 6, 7, 2, 4, 5];
+const setOfElements = new Set();
+for (const element of array) {
+    setOfElements.add(element); 
+}
+// console.table(setOfElements);
+
+const arrayOfUniqueElements =  [...new Set(array)];
+console.log(arrayOfUniqueElements);
