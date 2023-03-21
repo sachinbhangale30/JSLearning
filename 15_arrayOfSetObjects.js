2af0c8cffba21045b2e850e1cdaf375606c49bde
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-21 21:22:00 +0530

    Created 15_arrayOfSetObjects
    Created 15_arrayOfSetObjects

---
+++ 15_arrayOfSetObjects.js
@@ -0,0 +1,30 @@
+class Employee {
+    constructor(emp_id,emp_name,emp_dept,emp_salary,emp_company){
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_dept = emp_dept;
+        this.emp_salary = emp_salary;
+        this.emp_company = emp_company;
+    }
+}
+
+const emp_anil = new Employee(22,"Anil","IT",50000,"TCS");
+const emp_radha = new Employee(33,"Radha","HR",74000,"Wipro");
+const emp_rishi = new Employee(55,"Rishi","Finance",47000,"TCS");
+const emp_sonali = new Employee(66,"Sonali","Finance",45000,"Infy");
+const emp_monika = new Employee(77,"Monika","IT",40000,"Wipro");
+const emp_viny = new Employee(88,"Vinayak","IT",75000,"TCS");
+const emp_mahi = new Employee(99,"Mahesh","HR",85000,"Infy");
+
+const setOfEmployee = new Set();
+setOfEmployee.add(emp_anil);
+setOfEmployee.add(emp_radha);
+setOfEmployee.add(emp_rishi);
+setOfEmployee.add(emp_sonali);
+setOfEmployee.add(emp_monika);
+setOfEmployee.add(emp_viny);
+setOfEmployee.add(emp_mahi);
+
+setOfEmployee.forEach(employee => {
+    console.log(employee);
+});
