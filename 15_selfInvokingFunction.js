093b4b926bd3ed96756464dcfc4da94ddb7d0f52
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-21 16:36:21 +0530

    Created 15_selfInvokingFunction
    Created 15_selfInvokingFunction

---
+++ 15_selfInvokingFunction.js
@@ -0,0 +1,5 @@
+// Self Invoking Function or Immediately invoked function Expression
+( function(){
+    console.log("Inside Self Invoking Function or Immediately invoked function Expression");
+}) ();
+
