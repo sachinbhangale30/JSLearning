6609b1324a66a4ed4ef9f1c38ac095b7c9fd11fb
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-25 22:44:48 +0530

    Created 14_higherOrder
    Created 14_higherOrder

---
+++ 14_higherOrder.js
@@ -0,0 +1,18 @@
+function doHomerWork(callback){ 
+    console.log("Doing homework.. "); 
+    console.log("Finally Homework is completed..."); 
+    callback(); 
+    let submitHomeWork = function (){ 
+        console.log("Hey we both completed homework"); 
+        console.log("Let us submit please..."); 
+    } 
+    return submitHomeWork; 
+} 
+function copyHomework() { 
+    console.log("Copying homework..."); 
+    console.log("Thank you,, I copied homework..."); 
+} 
+doHomerWork(copyHomework);
+
+const result = doHomerWork(copyHomework);
+result();
