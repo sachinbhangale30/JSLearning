fb4343d846787b1e480aa04f78a10e579ce6ac8b
sachinbhangale30 <sachinbhangale30@gmail.com>
2023-03-31 19:32:38 +0530

    Created 02_addHobbies0B
    Created 02_addHobbies0B

---
+++ DOM/02_addHobbies0B.js
@@ -0,0 +1,5 @@
+const addHobbies = document.querySelector("#addHobbies");
+addHobbies.addEventListener("click",() => {
+const promptModal = prompt("Enter Your Hobbies","Hobbies");
+console.log(promptModal);
+});
